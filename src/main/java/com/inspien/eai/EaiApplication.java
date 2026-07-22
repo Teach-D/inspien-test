5c391b451af3c44fbd43bc64fe4bc35afaea6cc0
unknown <wkadht0619@gmail.com>
2026-07-22 11:22:56 +0900

    chore: Spring Boot 진입점 추가

---
+++ src/main/java/com/inspien/eai/EaiApplication.java
@@ -0,0 +1,11 @@
+package com.inspien.eai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EaiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EaiApplication.class, args);
+    }
+}
